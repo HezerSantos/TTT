075709ce974e17c42b9a3d1489d512fbf34ed2d2
HezerSantos <HezerSantos24@gmail.com>
2024-10-15 15:06:05 -0500

    Finished a little lol

--- js.js
+++ js.js
@@ -28,21 +28,39 @@ function checkBoard(board) {
     const gameSolutions = solutions();
     
     for (const solution of Object.values(gameSolutions)) {
-        const xCount = solution.reduce((count, index) => count + (board.gameBoard[index] === 'x' ? 1 : 0), 0);
-        const oCount = solution.reduce((count, index) => count + (board.gameBoard[index] === 'o' ? 1 : 0), 0);
-        console.log(xCount)
-        console.log()
-        console.log(oCount)
+        const xCount = solution.reduce((count, index) => count + (board.gameBoard[index] === 'X' ? 1 : 0), 0);
+        const oCount = solution.reduce((count, index) => count + (board.gameBoard[index] === 'O' ? 1 : 0), 0);
         if (xCount === 3) {
-            console.log("x wins");
-            return true; // Exit the function if 'x' wins
+            displayWinner("X's");
+            seeWinner(solution);
+            return true; 
         } else if (oCount === 3) {
-            console.log("o wins");
-            return true; // Exit the function if 'o' wins
+            displayWinner("O's");
+            seeWinner(solution);
+            return true;
         }
     }
 }
 
+function seeWinner(solution){
+    const buttons = getGameBoardButton();
+    solution.forEach(index => {
+        buttons.posList[index].style.cssText = `
+            background-color: lightBlue;
+        `;
+
+    })
+}
+
+function displayWinner(winner){
+    const container = document.querySelector(".winnerMessage");
+    container.textContent = `${winner} wins`;
+}
+
+function resetWinner(){
+    const container = document.querySelector(".winnerMessage");
+    container.textContent = '';
+}
 
 function solutions() {
     const solutionOne = [0, 1 ,2]
@@ -62,19 +80,115 @@ function solutions() {
 }
 
 
-(function () {
-    const playerOne = prompt("Enter player one name: ");
-    const playerOneMarker = prompt("Enter p1 marker: ");
-    const playerTwo = prompt("Enter player two name: ");
-    const playerTwoMarker = prompt("Enter p2 marker: ");
+function getGameBoardButton(){
+    const posOne = document.querySelector(".bOne");
+    const posTwo = document.querySelector(".bTwo");
+    const posThree = document.querySelector(".bThree");
+    const posFour = document.querySelector(".bFour");
+    const posFive = document.querySelector(".bFive");
+    const posSix = document.querySelector(".bSix");
+    const posSeven = document.querySelector(".bSeven");
+    const posEight = document.querySelector(".bEight");
+    const posNine = document.querySelector(".bNine");
 
-    game = startGame(playerOne, playerOneMarker, playerTwo, playerTwoMarker);
+    const posList = [posOne, posTwo, posThree,
+                     posFour, posFive, posSix,
+                     posSeven, posEight, posNine
+    ];
 
-    while (!checkBoard(game.playerBoard)) {
-        playerOneChoice = prompt("P1 place: ")
-        placeMarker(game.playerOne.marker, game.playerBoard, playerOneChoice)
-        playerTwoChoice = prompt("P2 place: ")
-        placeMarker(game.playerTwo.marker, game.playerBoard, playerTwoChoice)
+    return {posOne, posTwo, posThree,
+            posFour, posFive, posSix,
+            posSeven, posEight, posNine, posList
     }
+}
 
-})();
+function displayBoard(board){
+    let buttons = getGameBoardButton()
+    for (let i = 0; i < buttons.posList.length; i++) {
+        board.gameBoard[i] = buttons.posList[i].textContent
+    }
+}
+
+
+function updateBoard(board){
+    displayBoard(board);
+    checkBoard(board);
+}
+
+function resetBoard(board){
+    let buttons = getGameBoardButton()
+    for (let i = 0; i < buttons.posList.length; i++) {
+        board.gameBoard[i] = '';
+        buttons.posList[i].textContent = '';
+        buttons.posList[i].style.backgroundColor = '';
+    }
+}
+
+function displayTurn(marker){
+    const turnMessage = document.querySelector(".turnMessage");
+    turnMessage.textContent = `${marker}'s Turn`;
+}
+
+function displayDraw(){
+    const turnMessage = document.querySelector(".turnMessage");
+    turnMessage.textContent = "Draw";
+}
+
+function lockGame(){
+    const gameLock= document.querySelector(".lockGame");
+    gameLock.style.zIndex = '2';
+}
+
+function resetLock(){
+    const gameLock= document.querySelector(".lockGame");
+    gameLock.style.zIndex = '-1';
+}
+
+function getRoundNumber(){
+    return 0
+}
+
+const startButton = document.querySelector("#startGame");
+const resetButton = document.querySelector("#resetGame")
+
+const game = startGame('bob', 'X', 'jose', 'O');
+
+function playGame(){
+    const buttons = getGameBoardButton();
+    let round = 0;
+    buttons.posList.forEach(button => {
+        button.addEventListener('click', () => {
+            if (!button.textContent) {
+                if (round % 2 === 0){
+                    displayTurn(game.playerTwo.marker)
+                    button.textContent = game.playerOne.marker;
+                    updateBoard(game.playerBoard);
+                } else {
+                    displayTurn(game.playerOne.marker)
+                    button.textContent = game.playerTwo.marker;
+                    updateBoard(game.playerBoard);
+                }
+                round ++;
+                if (checkBoard(game.playerBoard)){
+                    lockGame();
+                }
+                if (!checkBoard(game.playerBoard) && round === 9){
+                    displayDraw();
+                }
+            }
+        })
+    })
+}
+
+startButton.addEventListener('click', () => {
+    playGame();
+})
+
+resetButton.addEventListener('click', () => {
+    resetBoard(game.playerBoard);
+    resetWinner();
+    resetLock();
+    playGame();
+})
+
+
